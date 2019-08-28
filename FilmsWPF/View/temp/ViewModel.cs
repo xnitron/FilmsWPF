4a469b108ee64e34d281f70c43a214937af231c0
vrexpro <vrexpro@gmail.com>
2019-08-28 23:37:09 +0300

    temp

---
+++ FilmsWPF/View/temp/ViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<string> Items { get; set; }
+       
+        public ViewModel()
+        {
+            Items = new ObservableCollection<string>() { "afd", "bbbb", "cccc"};
+            IsActive = false;
+        }
+
+        bool isActive;
+        public bool IsActive
+        {
+            get
+            {
+                return isActive;
+            }
+            set
+            {
+                isActive = value;
+                OnPropertyChanged("IsActive");
+            }
+        }
+        private string itembox;
+        public string Itembox
+        {
+            get { return itembox; }
+            set
+            {
+                if (value != itembox)
+                {
+                    itembox = value;
+                    foreach (var item in Items)
+                    {
+                        if(item.Contains(itembox))
+                        {
+                            IsActive = true;
+                        }
+                    }
+                    OnPropertyChanged("ItemBox");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string s)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(s));
+            }
+        }
+    }
+}
